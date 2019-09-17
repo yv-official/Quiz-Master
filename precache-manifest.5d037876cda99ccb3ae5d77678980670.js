2e762a52506d2fe29e7227568d6bdf3b8359a591
Yashveer Talan <yashveertalan@gmail.com>
2019-09-17 14:52:01 +0530

    Updates

---
+++ precache-manifest.5d037876cda99ccb3ae5d77678980670.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b893615183a6b560036410b9957c54df",
+    "url": "/Quiz-Master/index.html"
+  },
+  {
+    "revision": "37f801fb1efc345a27c5",
+    "url": "/Quiz-Master/static/css/main.7abf5bdf.chunk.css"
+  },
+  {
+    "revision": "bb3adbd1d4636181b770",
+    "url": "/Quiz-Master/static/js/2.a40cf2a4.chunk.js"
+  },
+  {
+    "revision": "37f801fb1efc345a27c5",
+    "url": "/Quiz-Master/static/js/main.828cd241.chunk.js"
+  },
+  {
+    "revision": "cab2e2cf7e0c3187c411",
+    "url": "/Quiz-Master/static/js/runtime~main.36fcaba3.js"
+  }
+]);
